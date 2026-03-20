e84e241e7864a674e219e48e5a3825a5a8856afe
Peety <dubman06@gmail.com>
2026-03-20 13:02:50 +0100

    refactor: remove AutoAttackSystem and HealthSystem; fixes Update TakeDamage on health component and basic attack component Fixes #3

--- Assets/Scripts/HealthBarUi.cs
+++ Assets/Scripts/HealthBarUi.cs
@@ -1,69 +1,69 @@
-using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
+// using UnityEngine;
+// using UnityEngine.UI;
+// using TMPro;
 
-public class HealthBarUI : MonoBehaviour
-{
-    [SerializeField] private HealthSystem healthSystem;
-    [SerializeField] private Image fillImage;
-    [SerializeField] private TextMeshProUGUI healthText; // Optional
-    [SerializeField] private Canvas canvas;
+// public class HealthBarUI : MonoBehaviour
+// {
+//     [SerializeField] private HealthComponent healthSystem;
+//     [SerializeField] private Image fillImage;
+//     [SerializeField] private TextMeshProUGUI healthText; // Optional
+//     [SerializeField] private Canvas canvas;
 
-    [Header("UI Settings")]
-    [SerializeField] private bool useFixedRotation = true;
-    [SerializeField] private Vector3 fixedRotation = new Vector3(45, 0, 0);
+//     [Header("UI Settings")]
+//     [SerializeField] private bool useFixedRotation = true;
+//     [SerializeField] private Vector3 fixedRotation = new Vector3(45, 0, 0);
 
-    private Camera mainCamera;
+//     private Camera mainCamera;
 
-    private void Start()
-    {
-        if (healthSystem != null)
-        {
-            healthSystem.OnHealthChangedEvent += UpdateHealthBar;
-            UpdateHealthBar(healthSystem.CurrentHealth, healthSystem.MaxHealth);
-        }
+//     private void Start()
+//     {
+//         if (healthSystem != null)
+//         {
+//             healthSystem.OnHealthChangedEvent += UpdateHealthBar;
+//             UpdateHealthBar(healthSystem.CurrentHealth, healthSystem.MaxHealth);
+//         }
 
-        mainCamera = Camera.main;
+//         mainCamera = Camera.main;
 
-        if (canvas != null)
-        {
-            canvas.renderMode = RenderMode.WorldSpace;
-        }
-    }
+//         if (canvas != null)
+//         {
+//             canvas.renderMode = RenderMode.WorldSpace;
+//         }
+//     }
 
-    private void LateUpdate()
-    {
-        if (canvas == null) return;
+//     private void LateUpdate()
+//     {
+//         if (canvas == null) return;
 
-        if (useFixedRotation)
-        {
-            canvas.transform.rotation = Quaternion.Euler(fixedRotation);
-        }
-        else if (mainCamera != null)
-        {
-            canvas.transform.LookAt(canvas.transform.position + mainCamera.transform.rotation * Vector3.forward,
-                mainCamera.transform.rotation * Vector3.up);
-        }
-    }
+//         if (useFixedRotation)
+//         {
+//             canvas.transform.rotation = Quaternion.Euler(fixedRotation);
+//         }
+//         else if (mainCamera != null)
+//         {
+//             canvas.transform.LookAt(canvas.transform.position + mainCamera.transform.rotation * Vector3.forward,
+//                 mainCamera.transform.rotation * Vector3.up);
+//         }
+//     }
 
-    private void UpdateHealthBar(float currentHealth, float maxHealth)
-    {
-        if (fillImage != null)
-        {
-            fillImage.fillAmount = currentHealth / maxHealth;
-        }
+//     private void UpdateHealthBar(float currentHealth, float maxHealth)
+//     {
+//         if (fillImage != null)
+//         {
+//             fillImage.fillAmount = currentHealth / maxHealth;
+//         }
 
-        if (healthText != null)
-        {
-            healthText.text = $"{Mathf.CeilToInt(currentHealth)}/{Mathf.CeilToInt(maxHealth)}";
-        }
-    }
+//         if (healthText != null)
+//         {
+//             healthText.text = $"{Mathf.CeilToInt(currentHealth)}/{Mathf.CeilToInt(maxHealth)}";
+//         }
+//     }
 
-    private void OnDestroy()
-    {
-        if (healthSystem != null)
-        {
-            healthSystem.OnHealthChangedEvent -= UpdateHealthBar;
-        }
-    }
-}
+//     private void OnDestroy()
+//     {
+//         if (healthSystem != null)
+//         {
+//             healthSystem.OnHealthChangedEvent -= UpdateHealthBar;
+//         }
+//     }
+// }
