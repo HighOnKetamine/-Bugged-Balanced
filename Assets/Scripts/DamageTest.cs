fa33efdec97453ffb3dc73d21cf549bc7991689b
HighOnKetamine <PavelBilekosob@gmail.com>
2026-04-16 08:39:54 +0200

    nah id commit

---
+++ Assets/Scripts/DamageTest.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class DamageTest : MonoBehaviour
+{
+    [Header("Settings")]
+    public GameObject damagePrefab;
+    public float heightOffset = 2.5f;
+    public float jitterAmount = 0.5f;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnDamageNumber();
+        }
+    }
+
+    void SpawnDamageNumber()
+    {
+        if (damagePrefab == null) return;
+
+        float randomX = Random.Range(-jitterAmount, jitterAmount);
+        float randomZ = Random.Range(-jitterAmount, jitterAmount);
+
+        Vector3 spawnPos = transform.position + new Vector3(randomX, heightOffset, randomZ);
+
+        GameObject go = Instantiate(damagePrefab, spawnPos, Quaternion.identity);
+
+        if (go.TryGetComponent<FloatingDmg>(out FloatingDmg dmgScript))
+        {
+            dmgScript.SetDamageValue(Random.Range(10, 100));
+        }
+    }
+}
